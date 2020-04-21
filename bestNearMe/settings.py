71ec98fd2f3d8c86d5d492293658d07de1bdccf9
BalrokHS <kostas.lianos.96@gmail.com>
2020-04-22 02:57:36 +0300

    Added templates and a basic navigation

--- bestNearMe/settings.py
+++ bestNearMe/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'bestNearMe.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'mainapp/templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
