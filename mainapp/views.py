59f6ccd3347471a53506a0f3dbac8a8a9867de03
klianos <kostas.lianos.96@gmail.com>
2020-05-01 22:17:35 +0300

    Added register and login working pages. We have to work on the beautifying part

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from mainapp.forms import RegisterForm
 
 
 # Create your views here.
+@login_required
 def index(request):
     return render(request, 'map.html')
 
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('/')
+    else:
+        form = RegisterForm()
+    return render(request, 'registration/register.html', {'form': form})
