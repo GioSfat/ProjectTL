199f6c3ced548c8c3ce2773c4153cf713c2a3a9c
GioSfat <giorgossfatidis@gmail.com>
2020-05-01 21:37:44 +0300

    Changes in models.py

--- mainapp/models.py
+++ mainapp/models.py
@@ -11,15 +11,14 @@ class Users(models.Model):
     email = models.EmailField(max_length=70)
     birthDate = models.DateField(null=True, blank=True)
 
-    class Admin:
+    class Reviewer:
         name = models.AutoField()
 
     class Customer:
         customerName = models.AutoField()
 
     class Owner:
-        busName = models.CharField(max_length=100)
-
+        busName = models.CharField(max_length=10)
 
 
 class Coordinates(models.Model):
@@ -28,7 +27,7 @@ class Coordinates(models.Model):
     add = models.CharField(max_length=70, null=True)
     city = models.CharField(max_length=70, null=True)
     coord_id = models.AutoField(primary_key=True, editable=False)  # na koitaksoyme an kanei swsto crossing
-    # coordID = models.CharField(max_length=70)
+    
 
 
 
@@ -54,10 +53,9 @@ class Business(models.Model):
     bsuSchedule = models.CharField(max_length=10,
                                    choices=[(tag, tag.value) for tag in Schedule])
     multimedia = models.FileField(editable=True)  # stin parenthesi tha valoume (upload_to="fakelos gia photos")
-    coordID = models.ForeignKey(Coordinates.coord_id,
-                                on_delete=models.CASCADE)  # na koitaksoyme an kanei swsto crossing
+    # coordID = models.ForeignKey(Coordinates.coord_id, on_delete=models.CASCADE) # na koitaksoyme an kanei swsto crossing
     # resID = models.ForeignKey()
-    ownerID = models.ForeignKey(Users.username, on_delete=models.CASCADE)
+    # ownerID = models.ForeignKey(Users.username, on_delete=models.CASCADE)
     tags = models.CharField(max_length=10,
                             choices=[(tag, tag.value) for tag in Preferences])
 
@@ -76,15 +74,16 @@ class Business(models.Model):
 class Reviews(models.Model):
     comments = models.TextField()
     ratings = models.PositiveIntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(100)])
-    busID = models.ForeignKey(Business.busID, null=False, blank=False, editable=False, on_delete=models.CASCADE)
+    # busID = models.ForeignKey(Business.busID, null=False, blank=False, editable=False, on_delete=models.CASCADE)
 
 
 
 class Reservations(models.Model):
     day = models.DateTimeField()
     resID = models.AutoField(primary_key=True, editable=False)
-    busID = models.ForeignKey(Business.busID, on_delete=models.CASCADE)
-    customerID = models.ForeignKey(Users.Customer, on_delete=models.CASCADE)
+    # busID = models.ForeignKey(Business.busID, on_delete=models.CASCADE)
+    # customerID = models.ForeignKey(Users.Customer, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
 
     @classmethod
     def book_res(cls, day):
