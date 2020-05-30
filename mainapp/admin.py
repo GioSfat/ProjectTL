f39436e355c27ee0a83b5a132ac72167002d41d6
klianos <kostas.lianos.96@gmail.com>
2020-05-30 17:23:10 +0300

    added models

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import User, Coordinates
+from .models import User, Coordinates, Business, Reviews
 
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Coordinates)
+admin.site.register(Business)
+admin.site.register(Reviews)
--- mainapp/models.py
+++ mainapp/models.py
@@ -92,22 +92,23 @@ class Schedule(Enum):
     night = "19:00 - 24:00"
 
 
-# class Business(models.Model):
-#     busName = models.CharField(max_length=100)
-#     busID = models.AutoField(primary_key=True, editable=False)
-#     phoneNum = models.IntegerField(max_length=10, null=True, blank=True)
-#     busEmail = models.EmailField(max_length=150)
-#     busSite = models.CharField(max_length=100)
-#     bsuSchedule = models.CharField(max_length=10,
-#                                    choices=[(tag, tag.value) for tag in Schedule])
-#     multimedia = models.FileField(editable=True)  # stin parenthesi tha valoume (upload_to="fakelos gia photos")
-#     coordID = models.ForeignKey(Coordinates.coord_id,
-#                                 on_delete=models.CASCADE)  # na koitaksoyme an kanei swsto crossing
-#     # resID = models.ForeignKey()
-#     ownerID = models.ForeignKey(User.username, on_delete=models.CASCADE)
-#     tags = models.CharField(max_length=10,
-#                             choices=[(tag, tag.value) for tag in Preferences])
-#
+class Business(models.Model):
+    busName = models.CharField(max_length=100)
+    busID = models.AutoField(primary_key=True, editable=False)
+    phoneNum = models.IntegerField(max_length=10, null=True, blank=True)
+    busEmail = models.EmailField(max_length=150)
+    busSite = models.CharField(max_length=100)
+    bsuSchedule = models.CharField(max_length=10,
+                                   choices=[(tag, tag.value) for tag in Schedule])
+    multimedia = models.FileField(editable=True)  # stin parenthesi tha valoume (upload_to="fakelos gia photos")
+    coordID = models.ForeignKey(Coordinates,
+                                on_delete=models.CASCADE)  # na koitaksoyme an kanei swsto crossing
+    # resID = models.ForeignKey()
+    ownerID = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.CharField(max_length=10,
+                            choices=[(tag, tag.value) for tag in Preferences])
+
+
 #     class Restaurant:
 #         pass
 #
@@ -118,11 +119,11 @@ class Schedule(Enum):
 #         pass
 #
 
-# class Reviews(models.Model):
-#     comments = models.TextField()
-#     ratings = models.PositiveIntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(100)])
-#     busID = models.ForeignKey(Business.busID, null=False, blank=False, editable=False, on_delete=models.CASCADE)
-#
+class Reviews(models.Model):
+    comments = models.TextField()
+    ratings = models.PositiveIntegerField(default=0, validators=[MinValueValidator(1), MaxValueValidator(100)])
+    busID = models.ForeignKey(Business, null=False, blank=False, editable=False, on_delete=models.CASCADE)
+
 #
 # class Reservations(models.Model):
 #     day = models.DateTimeField()
@@ -134,4 +135,3 @@ class Schedule(Enum):
 #     @classmethod
 #     def book_res(cls, day):
 #         pass
-
