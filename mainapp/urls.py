12a55fd8e8c5168d63bb1de71e881fd4b38a5be8
klianos <kostas.lianos.96@gmail.com>
2020-06-09 15:55:31 +0300

    Added templates and css files and implemented the views

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,4 +6,11 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('register/', views.register, name='register'),
     path('', include('django.contrib.auth.urls')),
+    path('logout/', views.logout, name='logout'),
+    path('bus_pg/', views.business_page, name='business_page'),
+    path('change_info_cus/', views.change_info_cus, name='change_info_cus'),
+    path('change_info_bus/', views.change_info_bus, name='change_info_bus'),
+    path('customer/', views.customer, name='customer'),
+    path('bus_sbm/', views.business_submit, name='business_submit'),
+
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
@@ -8,7 +8,7 @@ from mainapp.forms import RegisterForm
 # Create your views here.
 @login_required
 def index(request):
-    return render(request, 'map.html')
+    return render(request, 'SearchByDistance.html')
 
 
 def register(request):
@@ -24,3 +24,26 @@ def register(request):
     else:
         form = RegisterForm()
     return render(request, 'registration/register.html', {'form': form})
+
+def logout(request):
+    logout(request)
+# na mpei edo to main page 
+@login_required
+def business_page(request):
+    return render(request, 'Business_Page.html')
+
+@login_required
+def change_info_cus(request):
+    return render(request, 'Change_Info_Cus.html')
+
+@login_required
+def change_info_bus(request):
+    return render(request, 'ChangeBusinessInfo.html')
+
+@login_required
+def customer(request):
+    return render(request, 'CustomerPage.html')
+
+@login_required
+def business_submit(request):
+    return render(request, 'NewBusinessSubmit.html')
